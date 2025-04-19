67da6cb7e9d27ba34bf1567c4cf9afb881e563a4
mikheyev <alexey.mikheyev@kaspi.kz>
2025-04-19 12:57:52 +0500

    added reset

--- src/main/java/org/axzarian/timebot/service/impl/TelegramWebhookServiceImpl.java
+++ src/main/java/org/axzarian/timebot/service/impl/TelegramWebhookServiceImpl.java
@@ -2,6 +2,7 @@ package org.axzarian.timebot.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.axzarian.timebot.model.domain.Stopwatch;
 import org.axzarian.timebot.sender.TelegramSender;
 import org.axzarian.timebot.service.TelegramUpdateService;
 import org.axzarian.timebot.service.TelegramWebhookService;
@@ -15,6 +16,7 @@ public class TelegramWebhookServiceImpl implements TelegramWebhookService {
 
     private final TelegramUpdateService telegramUpdateService;
     private final TelegramSender        telegramSender;
+    private final Stopwatch             stopwatch;
 
 
     @Override
@@ -44,6 +46,8 @@ public class TelegramWebhookServiceImpl implements TelegramWebhookService {
 
             switch (data) {
                 case "refresh" -> telegramSender.editMessageForIvan(chatId, messageId, testResponse);
+                case "reset" -> { stopwatch.reset(); telegramSender.editMessageForIvan(chatId, messageId, testResponse);
+                }
             }
 
         }
